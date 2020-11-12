8ecef98ec57752532e29368db31f6c42971c4324
Monet Cooper <smomoc@monets-mbp-2.lan>
2020-11-11 22:12:59 -0500

    code complete

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import React from 'react'
 import './App.css';
+import Navbar from './components/Navbar';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'
+import Home from './components/pages/Home'
+import Services from './components/pages/Services';
+import Products from './components/pages/Products';
+import SignUp from './components/pages/SignUp';
 
 function App() {
-  return (
-    <div className='App'>
-      <h1>Hellp</h1>
-    </div>
-  );
+   return (
+      <>
+         <Router>
+            <Navbar/>
+            <Switch>
+               <Route exact path='/' component={Home}/>
+               <Route  path='/services' component={Services}/>
+               <Route  path='/products' component={Products}/>
+               <Route  path='/sign-up' component={SignUp}/>
+            </Switch>
+         </Router>
+      </>
+   );
 }
 
 export default App;
